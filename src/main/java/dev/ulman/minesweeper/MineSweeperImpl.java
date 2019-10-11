934b288091573bd5076b750f1318024efd847e3f
Marcin Ulman <marcin@ulman@dev>
2019-10-11 23:37:00 +0200

    initial commit

---
+++ src/main/java/dev/ulman/minesweeper/MineSweeperImpl.java
@@ -0,0 +1,11 @@
+package dev.ulman.minesweeper;
+
+public class MineSweeperImpl implements MineSweeper {
+    public void setMineField(String mineField) throws IllegalArgumentException {
+
+    }
+
+    public String getHintField() throws IllegalStateException {
+        return null;
+    }
+}
