f0f9f85df0b0f87829e9ab0ffc0f6b8f702828ca
Marcin Ulman <marcin@ulman@dev>
2019-10-12 21:49:02 +0200

    set Java version 1.8

--- src/main/java/dev/ulman/minesweeper/MineSweeperImpl.java
+++ src/main/java/dev/ulman/minesweeper/MineSweeperImpl.java
@@ -1,10 +1,13 @@
 package dev.ulman.minesweeper;
 
 public class MineSweeperImpl implements MineSweeper {
+
+    @Override
     public void setMineField(String mineField) throws IllegalArgumentException {
 
     }
 
+    @Override
     public String getHintField() throws IllegalStateException {
         return null;
     }
